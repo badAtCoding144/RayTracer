c6f425a2de02b2ddee564b7a7a1f0308a7718f55
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2025-01-31 20:10:38 +0200

    made it possible to move the camera by giving it its own set of tangent space vectors by having lookfrom, lookat and up vectors for the camera

--- Source/RayTracer/camera.h
+++ Source/RayTracer/camera.h
@@ -13,6 +13,10 @@ public:
 	int max_depth = 10; // max number of ray bounces into scene
 
 	double vfov = 90.0;       // Vertical field-of-view in degrees
+	point3 lookfrom = point3(0, 0, 0);
+	point3 lookat = point3(0, 0, -1);
+	vec3 vup = vec3(0, 1, 0);
+
 
 	void render(const hittable& world) {
 		initialize();
@@ -41,27 +45,31 @@ public:
 
 		pixel_samples_scale = 1.0 / samples_per_pixel;
 
-		center = point3(0, 0, 0);
+		center = lookfrom;
+
 
 		// Determine viewport dimensions.
-		auto focal_length = 1.0;
+		auto focal_length = (lookfrom - lookat).length();
 		auto theta = degrees_to_radians(vfov);
 		auto h = std::tan(theta / 2);
 		auto viewport_height = 2.0 * h;
 
 		auto viewport_width = viewport_height * (double(image_width) / image_height);
 
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+
 		// Calculate the vectors across the horizontal and down the vertical viewport edges.
-		auto viewport_u = vec3(viewport_width, 0, 0);
-		auto viewport_v = vec3(0, -viewport_height, 0);
+		auto viewport_u = viewport_width * u;
+		auto viewport_v = viewport_height * -v;
 
 		// Calculate the horizontal and vertical delta vectors from pixel to pixel.
 		pixel_delta_u = viewport_u / image_width;
 		pixel_delta_v = viewport_v / image_height;
 
 		// Calculate the location of the upper left pixel.
-		auto viewport_upper_left =
-			center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+		auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
 		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 	}
 
@@ -73,6 +81,7 @@ private:
 	vec3 pixel_delta_u;
 	vec3 pixel_delta_v;
 	double pixel_samples_scale;
+	vec3 u, v, w;
 
 	ray get_ray(int i, int j) const {
 		//construct a camera ray originating from the origin and directed at randomly sampled point on the pixel at i,j
