989d2e4da90d0f4f17b80a473fed27cb4092e0cb
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2025-02-03 15:03:20 +0200

    perlin noise marble texture with phase

--- Source/RayTracer/texture.h
+++ Source/RayTracer/texture.h
@@ -89,7 +89,7 @@ public:
 	noise_texture(double scale) : scale(scale) {}
 
 	color value(double u, double v, const point3& p) const override {
-		return color(1, 1, 1) * noise.turb(p, 7);
+		return color(0.5,0.5,0.5) * (1+ std::sin(scale*p.z()+10 * noise.turb(p,7)));
 	}
 private:
 	perlin noise;
