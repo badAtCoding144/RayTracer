e973d8be2c78f0aca0bb33dc51398963f29511bd
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2025-02-04 01:47:43 +0200

    cornell with boxes rotated around the y axis ting mabob

--- Source/RayTracer/quad.h
+++ Source/RayTracer/quad.h
@@ -4,6 +4,7 @@
 #include "hittable.h"
 #include <memory>
 #include "rtweekend.h"
+#include "hittable_list.h"
 
 class quad : public hittable {
 public: 
@@ -44,7 +45,7 @@ public:
 		auto beta = dot(w, cross(u, planar_hitpt_vector));
 
 		if (!is_interior(alpha, beta,rec)) return false;
-
+		 
 
 		//we hitted the quad
 		rec.t = t;
@@ -67,6 +68,10 @@ public:
 		rec.v = beta;
 		return true;
 	}
+
+
+
+
 private:
 	point3 Q;
 	vec3 u, v;
@@ -77,5 +82,30 @@ private:
 	double D;
 
 };
+
+
+inline shared_ptr<hittable_list> box(const point3& a, const point3& b, shared_ptr<material> mat)
+{
+	// Returns the 3D box (six sides) that contains the two opposite vertices a & b.
+
+	auto sides = make_shared<hittable_list>();
+
+	// Construct the two opposite vertices with the minimum and maximum coordinates.
+	auto min = point3(std::fmin(a.x(), b.x()), std::fmin(a.y(), b.y()), std::fmin(a.z(), b.z()));
+	auto max = point3(std::fmax(a.x(), b.x()), std::fmax(a.y(), b.y()), std::fmax(a.z(), b.z()));
+
+	auto dx = vec3(max.x() - min.x(), 0, 0);
+	auto dy = vec3(0, max.y() - min.y(), 0);
+	auto dz = vec3(0, 0, max.z() - min.z());
+
+	sides->add(make_shared<quad>(point3(min.x(), min.y(), max.z()), dx, dy, mat)); // front
+	sides->add(make_shared<quad>(point3(max.x(), min.y(), max.z()), -dz, dy, mat)); // right
+	sides->add(make_shared<quad>(point3(max.x(), min.y(), min.z()), -dx, dy, mat)); // back
+	sides->add(make_shared<quad>(point3(min.x(), min.y(), min.z()), dz, dy, mat)); // left
+	sides->add(make_shared<quad>(point3(min.x(), max.y(), max.z()), dx, -dz, mat)); // top
+	sides->add(make_shared<quad>(point3(min.x(), min.y(), min.z()), dx, dz, mat)); // bottom
+
+	return sides;
+}
 
 #endif
