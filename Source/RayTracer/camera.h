c55772beea6c299e344cb54fd0e9ea897c46a6b2
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2025-02-06 18:23:51 +0200

    wiat I never commited sewer

--- Source/RayTracer/camera.h
+++ Source/RayTracer/camera.h
@@ -31,9 +31,11 @@ public:
 			std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 			for (int i = 0; i < image_width; i++) {
 				color pixel_color(0, 0, 0);
-				for (int sample = 0; sample < samples_per_pixel; sample++) {
-					ray r = get_ray(i, j);
-					pixel_color += ray_color(r, max_depth ,world);
+				for (int s_j = 0; s_j < sqrt_spp; s_j++) {
+					for (int s_i = 0; s_i < sqrt_spp; s_i++) {
+						ray r = get_ray(i, j, s_i, s_j);
+						pixel_color += ray_color(r, max_depth, world);
+					}
 				}
 				write_color(std::cout, pixel_samples_scale*pixel_color);
 			}
@@ -44,6 +46,12 @@ public:
 	void initialize() {
 		image_height = int(image_width / aspect_ratio);
 		image_height = (image_height < 1) ? 1 : image_height;
+
+
+		sqrt_spp = int(std::sqrt(samples_per_pixel));
+		pixel_samples_scale = 1.0 / (sqrt_spp * sqrt_spp);
+		recip_sqrt_spp = 1.0 / sqrt_spp;
+
 
 		pixel_samples_scale = 1.0 / samples_per_pixel;
 
@@ -82,6 +90,10 @@ public:
 
 private:
 
+	int    sqrt_spp;             // Square root of number of samples per pixel
+	double recip_sqrt_spp;       // 1 / sqrt_spp
+
+
 	int image_height;
 	point3 center;
 	point3 pixel00_loc;
@@ -99,9 +111,11 @@ private:
 	}
 
 
-	ray get_ray(int i, int j) const {
-		//construct a camera ray originating from the origin and directed at randomly sampled point on the pixel at i,j
-		auto offset = sample_square();
+	ray get_ray(int i, int j, int s_i, int s_j) const {
+		// Construct a camera ray originating from the defocus disk and directed at a randomly
+		// sampled point around the pixel location i, j for stratified sample square s_i, s_j.
+
+		auto offset = sample_square_stratified(s_i, s_j);
 		auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 		auto ray_origin = (defocus_angle <=0) ? center : defocus_disk_sample();
 		auto ray_direction = pixel_sample - ray_origin;
@@ -141,6 +155,15 @@ private:
 		return color_from_emission + color_from_scatter;
 	}
 
+	vec3 sample_square_stratified(int s_i, int s_j) const {
+		// Returns the vector to a random point in the square sub-pixel specified by grid
+		// indices s_i and s_j, for an idealized unit square pixel [-.5,-.5] to [+.5,+.5].
+
+		auto px = ((s_i + random_double()) * recip_sqrt_spp) - 0.5;
+		auto py = ((s_j + random_double()) * recip_sqrt_spp) - 0.5;
+
+		return vec3(px, py, 0);
+	}
 
 };
 
